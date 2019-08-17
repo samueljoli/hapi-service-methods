53e4132a5bbf6cb6aa83d78a22e956b5f37b9f03
Samueljoli <samuel.joli.ftn@gmail.com>
2019-08-17 17:14:29 -0400

    complete implementation and tests

---
+++ test/index.js
@@ -0,0 +1,188 @@
+'use strict';
+
+const hapi = require('@hapi/hapi');
+const lab = require('@hapi/lab');
+
+const { script, assertions } = lab;
+const { describe, it } = exports.lab = script();
+assertions.should();
+
+const plugin = require('..');
+
+describe('Plugin', () => {
+
+    it('decorates server interface with registerServiceMethods util', async () => {
+        const server = hapi.Server();
+        (server.registerServiceMethods === undefined).should.equal(true);
+
+        await server.register(plugin);
+        server.registerServiceMethods.should.be.a('function');
+    });
+
+    describe('.registerServiceMethods()', () => {
+        it('accepts a single object argument and register methods to server under correct scope', async () => {
+            const server = hapi.Server();
+            const service = {
+                scope: 'sqs',
+                services: [
+                    {
+                        name: 'init',
+                        service: () => 'hello',
+                    },
+                ],
+            };
+            await server.register(plugin);
+
+            server.registerServiceMethods(service);
+
+            server.methods.sqs.init.should.be.a('function');
+        });
+
+        it('accepts a single array of objects and registers methods to server under correct scope', async () => {
+            const server = hapi.Server();
+            const services = [
+                {
+                    scope: 'sqs',
+                    services: [
+                        {
+                            name: 'init',
+                            service: () => 'hello',
+                        },
+                    ],
+                },
+                {
+                    scope: 'rabbitMq',
+                    services: [
+                        {
+                            name: 'init',
+                            service: () => 'hello',
+                        },
+                    ],
+                },
+            ];
+            await server.register(plugin);
+
+            server.registerServiceMethods(services);
+
+            server.methods.sqs.init.should.be.a('function');
+            server.methods.rabbitMq.init.should.be.a('function');
+        });
+
+        it('by default binds hapi server to "this" context', async () => {
+            const server = hapi.Server();
+            const service = {
+                scope: 'sqs',
+                services: [
+                    {
+                        name: 'init',
+                        service() {
+                            this.server.should.be.an('object');
+                        },
+                    },
+                ],
+            };
+            await server.register(plugin);
+
+            server.registerServiceMethods(service);
+
+            server.methods.sqs.init();
+        });
+
+        it('accepts an optional context property which will be used to bind to "this" context in server methods', async () => {
+            class SQS {}
+
+            const server = hapi.Server();
+            const service = {
+                scope: 'sqs',
+                services: [
+                    {
+                        name: 'init',
+                        service() {
+                            this.client.should.be.an('object');
+                            this.client.should.be.an.instanceOf(SQS);
+                        },
+                    },
+                ],
+                context: {
+                    client: new SQS(),
+                },
+            };
+            await server.register(plugin);
+
+            server.registerServiceMethods(service);
+
+            server.methods.sqs.init();
+        });
+    });
+
+    it('throws when scope is not provided', async () => {
+        const server = hapi.Server();
+        const services = [
+            {
+                services: [
+                    {
+                        name: 'init',
+                        service: () => 'hello',
+                    },
+                ],
+            },
+        ];
+        await server.register(plugin);
+
+        (() => {
+            server.registerServiceMethods(services);
+        }).should.throw(Error, '"scope" is required');
+    });
+
+    it('throws when services is not provided', async () => {
+        const server = hapi.Server();
+        const services = [
+            {
+                scope: 'scope',
+            },
+        ];
+        await server.register(plugin);
+
+        (() => {
+            server.registerServiceMethods(services);
+        }).should.throw(Error, '"services" is required');
+    });
+
+    it('throws when service name  is not provided', async () => {
+        const server = hapi.Server();
+        const services = [
+            {
+                scope: 'scope',
+                services: [
+                    {
+                        service: () => 'hello',
+                    },
+                ],
+            },
+        ];
+        await server.register(plugin);
+
+        (() => {
+            server.registerServiceMethods(services);
+        }).should.throw(Error, '"name" is required');
+    });
+
+    it('throws when service method is not provided', async () => {
+        const server = hapi.Server();
+        const services = [
+            {
+                scope: 'scope',
+                services: [
+                    {
+                        name: 'init',
+                    },
+                ],
+            },
+        ];
+        await server.register(plugin);
+
+        (() => {
+            server.registerServiceMethods(services);
+        }).should.throw(Error, '"service" is required');
+    });
+});
